74367cc7d85a67469206e3b545a88331ffc61adc
Ray Bergholm <raymond.bergholm@sanoma.com>
2018-10-15 16:00:13 +0300

    add tests for bad input to compose

--- tests/compose.test.js
+++ tests/compose.test.js
@@ -5,10 +5,19 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
-const should = chai.should;
 
 const { compose } = require("../src/compose");
 
 describe("calamari.compose unit tests", () => {
 
+    it("Empty input should throw an error", () => {
+        expect(compose()).to.eventually.be.rejectedWith("calamari.compose expects an array as an input");
+    });
+
+    it("Non-array input should throw an error", () => {
+        expect(compose("hi")).to.eventually.be.rejectedWith("calamari.compose expects an array as an input");
+        expect(compose({
+            hello: "world"
+        })).to.eventually.be.rejectedWith("calamari.compose expects an array as an input");
+    });
 });
